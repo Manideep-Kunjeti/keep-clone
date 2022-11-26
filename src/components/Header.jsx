8ebf4bc4461d84a6eec80de3d0157fb188b8f754
Manideep <85476837+Manideep-Kunjeti@users.noreply.github.com>
2022-11-27 01:09:38 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import NotesTwoToneIcon from "@material-ui/icons/NotesTwoTone";
+// import SpeakerNotesTwoToneIcon from "@material-ui/icons/SpeakerNotesTwoTone";
+import NoteAddTwoToneIcon from "@material-ui/icons/NoteAddTwoTone";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteAddTwoToneIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
